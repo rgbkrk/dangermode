732793f3687e144e61e7bab433ad79e2c5bb9507
Kyle Kelley <rgbkrk@gmail.com>
2023-04-09 16:28:02 -0700

    include the suggestion on request fail

---
+++ dangermode/suggestions.py
@@ -0,0 +1,5 @@
+"""Suggestions to help ChatGPT get unstuck"""
+
+RUN_CELL_PARSE_FAIL = """
+The run cell endpoint expects JSON like `{ "code": "import pandas as pd\nprint('hello world')" }`.
+""".strip()
